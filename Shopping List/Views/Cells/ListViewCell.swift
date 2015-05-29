84dcaa91902c8e488380d218f81c77b969e49169
Diego Haz <hazdiego@gmail.com>
2015-05-29 18:15:19 -0300

    Add share action for lists

--- Shopping List/Views/Cells/ListViewCell.swift
+++ Shopping List/Views/Cells/ListViewCell.swift
@@ -10,4 +10,12 @@ import UIKit
 
 class ListViewCell: ItemViewCell {
     @IBOutlet weak var itemsLabel: UILabel!
+    @IBOutlet weak var shareImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        shareImage.image = shareImage.image?.imageWithRenderingMode(.AlwaysTemplate)
+        shareImage.tintColor = UIColor.whiteColor()
+    }
 }
